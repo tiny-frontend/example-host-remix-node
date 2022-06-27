230df1cf9824b1af212dfddcbce2dc53eeb63555
adrienbaron <adrien@abaron.net>
2022-06-27 10:52:59 +0100

    upgrade ESLint

--- app/components/styled-anchor.tsx
+++ app/components/styled-anchor.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-export const StyledAnchor: React.FC<JSX.IntrinsicElements["a"]> = (props) => (
+export const StyledAnchor: React.FC<JSX.IntrinsicElements["a"]> = ({
+  children,
+  ...props
+}) => (
   <a
     {...props}
     className={`border-b-2 border-b-primary-base hover:border-b-primary-light transition-all ${
       props.className ?? ""
     }`}
-  />
+  >
+    {children}
+  </a>
 );
